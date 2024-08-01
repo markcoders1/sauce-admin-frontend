f88781d8abc153c5dda304651597cc2babb8338a
Noor Amin <noor.amin@markcoders.com>
2024-08-02 00:01:39 +0500

    authorization boiler plate code succefully putted

---
+++ src/Redux/Slice/UserSlice/UserSlice.js
@@ -0,0 +1,25 @@
+// Redux/Slice/UserSlice/UserSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'auth',
+  initialState :{
+  
+    accessToken: null,
+    refreshToken: null,
+    _id: null,
+    username: null,
+    email: null,
+    createdAt: null,
+    updatedAt: null,
+    authenticated: false,
+  },
+  reducers: {
+    handleAuth: (state, action) => {
+      return { ...state, ...action.payload };
+    },
+  },
+});
+
+export const { handleAuth } = userSlice.actions;
+export default userSlice.reducer;
